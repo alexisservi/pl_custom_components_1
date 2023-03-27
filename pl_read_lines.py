cb32a8574db1b5bbc4e58a72e79849ea3a0d700a
alexis <alexis.ortega@servinformacion.com>
2023-03-27 10:35:00 -0500

    understanding inputs

--- pl_read_lines.py
+++ pl_read_lines.py
@@ -9,6 +9,7 @@ from typing import NamedTuple
 
 URL_READ_LINES_COMP = 'gs://ml-auto-pipelines-bucket/components-yamls/line-reader-writer/kubeflow_component_spec.yaml'
 
+#---------------------------------------------------------------------------------------------------
 @dsl.component()
 def get_input_parameters(input_path_1: str, 
                          output_path_1: str,
@@ -29,6 +30,7 @@ def get_input_parameters(input_path_1: str,
     example_output = namedtuple('ExampleOutputs', ['input_path_1', 'lines_to_read_1'])
     return example_output(input_path_1, lines_to_read_1)
 
+#---------------------------------------------------------------------------------------------------
 @dsl.pipeline(name='custom-components-v1', description='A pipeline with custom components')
 def custom_components_pipeline(input_path_1: str = 'gs://ml-auto-pipelines-bucket/inputs/test_input_lines.txt',
                                output_path_1: str = 'gs://ml-auto-pipelines-bucket/inputs/test_output_lines.txt',
@@ -36,6 +38,7 @@ def custom_components_pipeline(input_path_1: str = 'gs://ml-auto-pipelines-bucke
 
     
     inp_comp = get_input_parameters(input_path_1=input_path_1, output_path_1=output_path_1, lines_to_read_1=lines_to_read_1)
+    
     read_lines_task01 = kfp.components.load_component_from_url(
         url=URL_READ_LINES_COMP)  # Passing pipeline parameter as argument to consumer op
     
