8b0c31fd8c7f8347469b1ec1baba9c8d3ba0f514
alexis <alexis.ortega@servinformacion.com>
2023-03-27 17:42:10 -0500

    understanding inputs

--- pl_read_lines.py
+++ pl_read_lines.py
@@ -60,7 +60,7 @@ def custom_components_pipeline(input_path_1: str = 'gs://ml-auto-pipelines-bucke
         url=URL_READ_LINES_COMP)  # Passing pipeline parameter as argument to consumer op
     
     test_input_string = 'gs://ml-auto-pipelines-bucket/inputs/test_input_lines.txt'
-    read_lines_task01(input_1=inp_comp.outputs["out_1"],
+    read_lines_task01(input_1=test_input_string, # inp_comp.outputs["out_1"],
                       #output_1= inp_comp.outputs["output_path_1"],
                       parameter_1=inp_comp.outputs["lines_to_read_1"]) 
 
