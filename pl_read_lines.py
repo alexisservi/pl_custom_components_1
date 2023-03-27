60f51dcfc3341d1cbb18a6ac7843ecf3d194fa3a
alexis <alexis.ortega@servinformacion.com>
2023-03-27 09:34:20 -0500

    understanding inputs

--- pl_read_lines.py
+++ pl_read_lines.py
@@ -14,14 +14,14 @@ def hello_world(text: str) -> str:
     return text
 
 @dsl.pipeline(name='custom-components-v1', description='A pipeline with custom components')
-def custom_components_pipeline(input_1: str = 'gs://ml-auto-pipelines-bucket/inputs/test_input_lines.txt',
-                               output_1: str = 'gs://ml-auto-pipelines-bucket/inputs/test_output_lines.txt',
-                               parameter_1: int = 5):
+def custom_components_pipeline(input_path_1: str = 'gs://ml-auto-pipelines-bucket/inputs/test_input_lines.txt',
+                               output_path_1: str = 'gs://ml-auto-pipelines-bucket/inputs/test_output_lines.txt',
+                               lines_to_read_1: int = 5):
 
     read_lines_task01 = kfp.components.load_component_from_url(
         url=URL_READ_LINES_COMP)  # Passing pipeline parameter as argument to consumer op
     
-    read_lines_task01(input_1=input_1, parameter_1=parameter_1) 
+    read_lines_task01(input_1=input_path_1, parameter_1=lines_to_read_1) 
 
 
 # V1 Compiler -> it works...!
