0fefe47d1d87d57a77c81e5896e482bd1da608b0
alexis <alexis.ortega@servinformacion.com>
2023-03-27 19:01:32 -0500

    understanding inputs

--- pl_read_lines.py
+++ pl_read_lines.py
@@ -48,21 +48,68 @@ def get_input_parameters(input_path_1: str,
     return example_output(lines_to_read_1, input_path_1)
 
 #---------------------------------------------------------------------------------------------------
+@dsl.component()
+def file_writer(lines_to_write_1: int,
+                out_file_1: OutputPath()) -> NamedTuple(
+  'ExampleOutputs',
+  [
+    ('lines_to_read', int),
+    ('test_string_out', str)
+  ]):
+    
+    N_LINES_TO_WRITE = 20
+    with open(out_file_1.path, 'w') as path_writer:
+        for k in range(lines_to_write_1):
+            if k == 0:
+                path_writer.write("Test file writing\n")
+            else:
+                path_writer.write(str(k) + "\n")
+
+    lines_to_read = 5
+    from collections import namedtuple
+    example_output = namedtuple('ExampleOutputs', ['lines_to_read', 'test_string_out'])
+    return example_output(lines_to_read, "produce_file_output test string")
+
+#---------------------------------------------------------------------------------------------------
+@dsl.component()
+def input_file_reader(file_path_1: InputPath,
+                        lines_to_read: int) -> NamedTuple(
+  'ExampleOutputs',
+  [
+    ('test_int_out', int),
+    ('test_string_out', str)
+  ]):
+    
+    with open(file_path_1.path, 'r') as path_reader:
+        for k, line in path_reader.readlines():
+            print("LINE: {}".format(line))
+            if k == (lines_to_read - 1):
+                break
+
+    input_path_1 = "file1.txt" #str(out_1.path)
+    from collections import namedtuple
+    example_output = namedtuple('ExampleOutputs', ['test_int_out', 'test_string_out'])
+    return example_output(29, "produce_file_output test string")
+
+#---------------------------------------------------------------------------------------------------
 @dsl.pipeline(name='custom-components-v1', description='A pipeline with custom components')
 def custom_components_pipeline(input_path_1: str = 'gs://ml-auto-pipelines-bucket/inputs/test_input_lines.txt',
                                output_path_1: str = 'gs://ml-auto-pipelines-bucket/inputs/test_output_lines.txt',
-                               lines_to_read_1: int = 5):
+                               lines_to_write_1: int = 37):
 
     
-    inp_comp = get_input_parameters(input_path_1=input_path_1, lines_to_read_1=lines_to_read_1)
+    file_writer_task = file_writer(lines_to_write_1=lines_to_write_1)
     
-    read_lines_task01 = kfp.components.load_component_from_url(
-        url=URL_READ_LINES_COMP)  # Passing pipeline parameter as argument to consumer op
+    """
+    read_lines_task01 = kfp.components.load_component_from_url(url=URL_READ_LINES_COMP)  # Passing pipeline parameter as argument to consumer op
     
     test_input_string = 'gs://ml-auto-pipelines-bucket/inputs/test_input_lines.txt'
     read_lines_task01(input_1=test_input_string, # inp_comp.outputs["out_1"],
                       #output_1= inp_comp.outputs["output_path_1"],
-                      parameter_1=inp_comp.outputs["lines_to_read_1"]) 
+                      parameter_1=inp_comp.outputs["lines_to_read_1"])
+    """
+    file_reader_task = input_file_reader(file_path_1=file_writer_task.outputs["out_file_1"], 
+                                         lines_to_read=file_writer_task.outputs["lines_to_read"])
 
 
 # V1 Compiler -> it works...!
