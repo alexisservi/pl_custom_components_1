38a85b09c86e8cd595da42f7da889dfb3a2a376d
alexis <alexis.ortega@servinformacion.com>
2023-03-29 09:49:25 -0500

    testing

--- pl_read_lines.py
+++ pl_read_lines.py
@@ -112,7 +112,7 @@ def custom_components_pipeline(input_path_1: str = 'gs://ml-auto-pipelines-bucke
     read_lines_task01 = kfp.components.load_component_from_url(url=URL_READ_LINES_COMP)  # Passing pipeline parameter as argument to consumer op
     
     test_input_string = 'gs://ml-auto-pipelines-bucket/inputs/test_input_lines.txt'
-    read_lines_task01(input_1=test_input_string, # file_writer_task.outputs["out_file_1"], # 
+    read_lines_task01(input_1=file_writer_task.outputs["out_file_1"], # 
                       parameter_1=file_writer_task.outputs["lines_to_read"])
     
 #------------------------------------------
@@ -120,7 +120,9 @@ def custom_components_pipeline(input_path_1: str = 'gs://ml-auto-pipelines-bucke
 # V1 Compiler -> it works...
 compiler.Compiler().compile(
     pipeline_func=custom_components_pipeline,
-    package_path='custom_components_pipeline.yaml')
+    package_path='custom_components_pipeline.yaml', 
+    #type_check=False
+    )
 
 print("List directory files")
 print(os.listdir())
